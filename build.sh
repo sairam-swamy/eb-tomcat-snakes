7612c1e4599400a3de9876e1ab23daffb46d91fd
Sairam Mandapaka <59683108+sairam-swamy@users.noreply.github.com>
2026-08-03 00:31:32 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,47 @@
+# cd src
+# mkdir -p WEB-INF/classes
+# echo .
+# javac -d WEB-INF/classes com/snakes/model/Media.java
+# echo .
+# javac -classpath WEB-INF/lib/*:WEB-INF/classes -d WEB-INF/classes com/snakes/model/Movie.java
+# echo .
+# javac -classpath WEB-INF/lib/*:WEB-INF/classes -d WEB-INF/classes com/snakes/web/ListMovies.java
+# echo .
+# javac -classpath WEB-INF/lib/*:WEB-INF/classes -d WEB-INF/classes com/snakes/web/AddMovie.java
+# echo .
+# javac -classpath WEB-INF/lib/*:WEB-INF/classes -d WEB-INF/classes com/snakes/web/SearchMovies.java
+# echo .
+# if [ -d ".ebextensions/httpd/conf.d" ]; then
+#   jar -cf ROOT.war *.jsp images css js WEB-INF .ebextensions/*.config .ebextensions/*.json .ebextensions/httpd/conf.d/*.conf
+# else
+#   jar -cf ROOT.war *.jsp images css js WEB-INF .ebextensions/*.config .ebextensions/*.json
+# fi
+# echo .
+# if [ -d "/Library/Tomcat/webapps" ]; then
+#   cp ROOT.war /Library/Tomcat/webapps
+#   echo .
+# fi
+# mv ROOT.war ../
+# echo .
+# echo "SUCCESS"
+
 cd src
 mkdir -p WEB-INF/classes
+mkdir -p WEB-INF/lib
+
+# Ensure any duplicate or old log4j-core versions are removed
+rm -f WEB-INF/lib/log4j-core-2.20.0.jar
+
 echo .
 javac -d WEB-INF/classes com/snakes/model/Media.java
 echo .
-javac -classpath WEB-INF/lib/*:WEB-INF/classes -d WEB-INF/classes com/snakes/model/Movie.java
+javac -classpath "WEB-INF/lib/*:WEB-INF/classes" -d WEB-INF/classes com/snakes/model/Movie.java
 echo .
-javac -classpath WEB-INF/lib/*:WEB-INF/classes -d WEB-INF/classes com/snakes/web/ListMovies.java
+javac -classpath "WEB-INF/lib/*:WEB-INF/classes" -d WEB-INF/classes com/snakes/web/ListMovies.java
 echo .
-javac -classpath WEB-INF/lib/*:WEB-INF/classes -d WEB-INF/classes com/snakes/web/AddMovie.java
+javac -classpath "WEB-INF/lib/*:WEB-INF/classes" -d WEB-INF/classes com/snakes/web/AddMovie.java
 echo .
-javac -classpath WEB-INF/lib/*:WEB-INF/classes -d WEB-INF/classes com/snakes/web/SearchMovies.java
+javac -classpath "WEB-INF/lib/*:WEB-INF/classes" -d WEB-INF/classes com/snakes/web/SearchMovies.java
 echo .
 if [ -d ".ebextensions/httpd/conf.d" ]; then
   jar -cf ROOT.war *.jsp images css js WEB-INF .ebextensions/*.config .ebextensions/*.json .ebextensions/httpd/conf.d/*.conf
@@ -24,4 +56,3 @@ fi
 mv ROOT.war ../
 echo .
 echo "SUCCESS"
-
